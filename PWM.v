3de9badf79306b480ef8a592d1ac50c0c174398a
Steven Ventura <8stevenventura@gmail.com>
2017-04-22 17:08:29 -0500

    yup

---
+++ PWM.v
@@ -0,0 +1,32 @@
+module PWM(clk,rst,enable,PWM_Out); 
+	parameter HIGH = 1'b1,
+		  LOW  = 1'b0,
+		  ZEROS = 8'b0,
+		  FULL = 8'b11111111;
+
+	input enable, clk, rst;
+	reg[7:0] counter = ZEROS;
+	output reg PWM_Out;
+
+	parameter speed = 8'h0f;
+	
+	always@(posedge clk) begin
+		if(rst == HIGH) begin
+			counter <= ZEROS;
+		end
+		else if(enable == HIGH) begin
+			if(counter == FULL) begin
+				counter <= ZEROS;
+			end
+			else if(counter < speed) begin
+				PWM_Out <= HIGH;
+			end
+			else if(counter >= speed) begin
+				PWM_Out <= LOW;
+			end
+			else ;
+			counter <= counter + 1;
+		end
+		else counter <= ZEROS;
+	end
+endmodule 
---
+++ Top.v
@@ -0,0 +1,34 @@
+/*
+Computer Archictecture project: Line following 2-wheeled robot
+5:05 PM Saturday, April 22, 2017
+
+Alamin Ahmed,
+Luis Robles,
+Vinh Vu,
+Daniel Lopez,
+and Steven Ventura
+
+*/
+
+module Top(clk,PWM_OutR, dirR,PWM_OutL, dirL,sensorLeftRaw, sensorMiddleRaw, sensorRightRaw);
+	input clk;//50MHz??
+	input sensorLeftRaw, sensorMiddleRaw, sensorRightRaw;
+	output PWM_OutR, dirR, PWM_OutL, dirL;
+	wire sensorLeftFiltered,sensorMiddleFiltered,sensorRightFiltered;
+	//TODO: use a slower clock for filters?
+	LightSensorFilter lsf1 (clk, sensorLeftRaw, sensorLeftFiltered);
+	LightSensorFilter lsf2 (clk, sensorMiddleRaw, sensorMiddleFiltered);
+	LightSensorFilter lsf3 (clk, sensorRightRaw, sensorRightFiltered);
+	
+	LineFollow lf1 (clk, 
+			sensorLeftFiltered,sensorRightFiltered,sensorMiddleFiltered,
+			
+			);
+	
+	
+	/*
+	this makes it go forward 3 seconds, backward 3 seconds, repeat.
+	motor right(clk,PWM_OutR);
+	motor left(clk,PWM_OutL);
+	delay myDelay(clk,rst,3,dirL,dirR);*/
+endmodule
